e0560feb5dae76494c4ee4b5dfbe980bb06eb653
Brian Dickens <brian@hostilefork.com>
2022-06-14 02:46:26 -0400

    Reorganize Cypress, move test
    It's less clutter to put all the cypress-related files into a
    subdirectory.  This puts them in %cypress/
    
    This also moves the script %rx-app.cy.js into this repository.
    It will be pulled by Ren-C when doing new web builds, and tested
    against before greenlighting the build.  But it should be edited
    and maintained here.

---
+++ cypress/cypress.config.js
@@ -0,0 +1,14 @@
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
+  projectId: "zpvrpm",  // humanistic @ cypress.io
+
+  e2e: {
+    specPattern: 'e2e/**/*.cy.{js,jsx,ts,tsx}',
+    supportFile: 'support/e2e.{js,jsx,ts,tsx}',
+
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+})
